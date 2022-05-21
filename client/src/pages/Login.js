0bc60aa3d1fe2a2fe089d369377809c4e4de9cdb
John Kommala <johnwesleygithub@gmail.com>
2022-05-21 15:29:11 +0530

    Heroku Deployment

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -27,7 +27,7 @@ const StyledLoginButton = styled.a`
 const LOGIN_URI =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8888/login"
-    : "https://spotics.vercel.app/login";
+    : "https://spotifics.herokuapp.com/login";
 
 const Login = () => (
   <StyledLoginContainer>
