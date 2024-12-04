2615ef0829822bed2c0cf57ce795ba213f4514f8
John Kommala <johnwesleygithub@gmail.com>
2024-12-04 18:17:07 +0530

    added env vars types

---
+++ server/environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLIENT_SECRET: string;
+      CLIENT_ID: string;
+      FRONTEND_URI: string;
+      REDIRECT_URI: string;
+      PORT?: number;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export { }
